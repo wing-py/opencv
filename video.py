3a513d7ef654b0658ee87c1c744daa024fadf1e6
wing-py <Wei3.1415926>
2020-10-12 19:49:35 +0800

    version 0

---
+++ video.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+import cv2 
+def videocap():
+    cap=cv2.VideoCapture(0)
+    while True:
+        ret,frame=cap.read()
+        #gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        cv2.imshow("videocap",frame)
+        k=cv2.waitKey(1)
+        if k==ord("q"):
+            break
+    cap.release()
+    cv22.destroyAllWindows()
